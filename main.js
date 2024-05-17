6e4fc4eb0836abc6259b4aab7a274501bd1effd1
kiddocoder <ndayikezatresor9@gmail.com>
2024-05-17 07:20:07 +0200

    main.js

--- main.js
+++ main.js
@@ -1,18 +1,3 @@
-const menuBtn = document.getElementById("menu-btn");
-const navLinks = document.getElementById("nav-links");
-const menuBtnIcon = menuBtn.querySelector("i");
-
-menuBtn.addEventListener("click", (e) => {
-  navLinks.classList.toggle("open");
-
-  const isOpen = navLinks.classList.contains("open");
-  menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
-});
-
-navLinks.addEventListener("click", (e) => {
-  navLinks.classList.remove("open");
-  menuBtnIcon.setAttribute("class", "ri-menu-line");
-});
 
 const scrollRevealOption = {
   distance: "50px",
@@ -31,7 +16,7 @@ ScrollReveal().reveal(".header__container .section__description", {
   ...scrollRevealOption,
   delay: 1000,
 });
-ScrollReveal().reveal(".header__container .header__btns", {
+ScrollReveal().reveal(".header__container", {
   ...scrollRevealOption,
   delay: 1500,
 });
