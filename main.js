631173a2324e0c1ce61d3c574874c8abe1e78cf5
kiddocoder <ndayikezatresor9@gmail.com>
2024-05-19 14:59:35 +0200

    3 commit

--- main.js
+++ main.js
@@ -34,10 +34,6 @@ Array.from(progressBar).forEach((bar) => {
   bar.querySelector("span").style.width = progress + "%";
 });
 
-ScrollReveal().reveal(".about__image img", {
-  ...scrollRevealOption,
-  origin: "left",
-});
 ScrollReveal().reveal(".about__content h4", {
   ...scrollRevealOption,
   delay: 500,
@@ -55,53 +51,10 @@ ScrollReveal().reveal(".service__card", {
   ...scrollRevealOption,
   interval: 500,
 });
-
-const tabList = document.querySelector(".resume__tablist");
-
-tabList.addEventListener("click", (e) => {
-  const tabIndex = e.target.dataset.tab;
-  if (!tabIndex) return;
-
-  const tabs = document.querySelectorAll("[data-tab]");
-  Array.from(tabs).forEach((tab) => {
-    if (tab.dataset.tab === tabIndex) {
-      tab.classList.add("active");
-    } else {
-      tab.classList.remove("active");
-    }
-  });
-
-  const activePanel = document.querySelector(".panel__grid.active");
-  const toActivePanel = document.querySelector(`[data-panel="${tabIndex}"]`);
-  if (activePanel.dataset.panel === tabIndex) return;
-  activePanel.classList.add("close");
-  activePanel.addEventListener(
-    "animationend",
-    (e) => {
-      activePanel.classList.remove("active");
-      activePanel.classList.remove("close");
-      toActivePanel.classList.add("active");
-    },
-    { once: true }
-  );
-});
 var swiperOptions = {
-  loop: true,
-  freeMode: true,
+  slidesPerView: "auto",
   spaceBetween: 30,
-  grabCursor: true,
-  slidesPerView: 'auto',
-  autoplayDisableOnInteraction: false,
   loop: true,
-  autoplay: {
-    enabled: true,
-    delay: 2,
-    pauseOnMouseEnter: true,
-    disableOnInteraction: false,
-  },
-  freeMode: true,
-  speed: 2500,
-  freeModeMomentum: false
 };
 const swiper = new Swiper(".swiper", swiperOptions);
 
